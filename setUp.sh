6e8883891811758a9b439694355fb84eec8dfd71
L Daniel De San Pedro V <ldanieldesanpedro@gmail.com>
2019-06-05 21:04:37 -0500

    Finished the first version of the scripts

--- setUp.sh
+++ setUp.sh
@@ -2,3 +2,19 @@ echo Setting Up Mac
 # First update everything
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew tap Homebrew/bundle
+
+#Time to install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+#Backup the zshrc file just in case
+cat ~/.zshrc > ./.zshrc_current
+
+#Restore the old zshrc
+cat ~/.zshrc_copy > ./.zshrc
+
+mkdir ~/setUpScripts
+
+curl https://raw.githubusercontent.com/DanielDes/setUpScripts/master/wrapUp.sh > ~/setUpScripts/wrapUp.sh
+
+#Generate ssh keys
+ ssh-keygen -t rsa -b 4096
--- wrapUp.sh
+++ wrapUp.sh
@@ -6,6 +6,10 @@ brew bundle dump
 #Make copy of zshrc file
 cat ~/.zshrc > ~/.zshrc_copy
 
-
+#Move the backup file to the backUp directory
 mv ./Brewfile ~/backUp
 mv ~/.zshrc_copy ~/backUp
+
+#Obtain the setUp script from the repository
+curl https://raw.githubusercontent.com/DanielDes/setUpScripts/master/setUp.sh > ~/backUp/setUp.sh
+chmod +x ~/backUp/setUp.sh
